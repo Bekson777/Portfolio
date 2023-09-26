f956044fc694a4e0b10d0b0f9ca0f91c0e6a54b3
Beka Nazarashvili <121035641+Bekson777@users.noreply.github.com>
2023-09-26 09:11:16 -0700

    update8

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
-import myResume from "../assets/Beqa-Nazarashvili-Resume.pdf";
+import myResume from "../assets/REsume-Beqa-Nazarashvili.pdf";
 
 const SocialLinks = () => {
   const links = [
